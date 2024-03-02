2159fe9302b06ae25050c8c95a23effe56a13a70
rayauguste <161899050+rayauguste@users.noreply.github.com>
2024-03-02 14:43:28 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -13,16 +13,20 @@ document.addEventListener('DOMContentLoaded', function() {
   document.addEventListener('DOMContentLoaded', function() {
     var hamburger = document.getElementById('hamburger');
     var element = document.querySelector('#burger-menu');
+    var closeButton = document.getElementById('close-button');
     var bodyClick = null; // declare a variable to store the div element
   
-    if (hamburger && element) {
+    // Check if all necessary elements exist
+    if (hamburger && element && closeButton) {
       hamburger.addEventListener('change', function() {
         console.log('Checkbox state changed');
         // Toggle the 'move-left' class based on the state of the checkbox
         if (hamburger.checked) {
           element.classList.remove('move-left');
+          closeButton.classList.remove('move-left'); // Remove 'move-left' from close button
         } else {
           element.classList.add('move-left');
+          closeButton.classList.add('move-left'); // Add 'move-left' to close button
         }
   
         // Check if the hamburger is checked and if the bodyClick exists
@@ -44,14 +48,10 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
   
-  
-  
-  
-  
 
 //Click off burger menu
 document.addEventListener('DOMContentLoaded', function() {
-  var elements = [document.querySelector('#burger-menu')];
+  var elements = [document.querySelector('#burger-menu'), document.querySelector('#close-button')];
 
   document.addEventListener("click", function(event) {
     if (event.target.id === "bodyClick") {
@@ -69,4 +69,26 @@ document.addEventListener('DOMContentLoaded', function() {
       bodyClick = null;
     }
   });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var closeButton = document.getElementById('close-button');
+  var elements = [document.querySelector('#burger-menu'), document.querySelector('#close-button')];
+
+  if (closeButton) {
+    closeButton.addEventListener('click', function() {
+      // Uncheck the checkbox
+      document.getElementById("hamburger").checked = false;
+      // Remove the class "move-left" from the elements
+      elements.forEach(function(element) {
+        element.classList.add("move-left");
+      });
+      // get the current div element by its id
+      var bodyClick = document.getElementById("bodyClick");
+      // remove the div element from the document body
+      document.body.removeChild(bodyClick);
+      // set the variable to null
+      bodyClick = null;
+    });
+  }
 });
