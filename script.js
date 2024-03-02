cead9df50398a55f9eab90247e609d9e646ad435
rayauguste <161899050+rayauguste@users.noreply.github.com>
2024-03-02 14:33:34 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -8,3 +8,65 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     });
   });
+
+  //burger menu check box
+  document.addEventListener('DOMContentLoaded', function() {
+    var hamburger = document.getElementById('hamburger');
+    var element = document.querySelector('#burger-menu');
+    var bodyClick = null; // declare a variable to store the div element
+  
+    if (hamburger && element) {
+      hamburger.addEventListener('change', function() {
+        console.log('Checkbox state changed');
+        // Toggle the 'move-left' class based on the state of the checkbox
+        if (hamburger.checked) {
+          element.classList.remove('move-left');
+        } else {
+          element.classList.add('move-left');
+        }
+  
+        // Check if the hamburger is checked and if the bodyClick exists
+        if (hamburger.checked) {
+          // create a new div element and assign an id
+          bodyClick = document.createElement('div');
+          bodyClick.id = 'bodyClick';
+          // apply the CSS style using style.cssText
+          bodyClick.style.cssText = "height: 100%; width: 100%; position: fixed; top: 0; left: auto; right: 0; z-index: 5;";
+          // append the div element to the document body
+          document.body.appendChild(bodyClick);
+        } else if (!hamburger.checked && bodyClick) {
+          // remove the div element from the document body
+          document.body.removeChild(bodyClick);
+          // set the variable to null
+          bodyClick = null;
+        }
+      });
+    }
+  });
+  
+  
+  
+  
+  
+
+//Click off burger menu
+document.addEventListener('DOMContentLoaded', function() {
+  var elements = [document.querySelector('#burger-menu')];
+
+  document.addEventListener("click", function(event) {
+    if (event.target.id === "bodyClick") {
+      // Uncheck the checkbox
+      document.getElementById("hamburger").checked = false;
+      // Remove the class "move-left" from the elements
+      elements.forEach(function(element) {
+        element.classList.add("move-left");
+      });
+      // get the current div element by its id
+      var bodyClick = document.getElementById("bodyClick");
+      // remove the div element from the document body
+      document.body.removeChild(bodyClick);
+      // set the variable to null
+      bodyClick = null;
+    }
+  });
+});
